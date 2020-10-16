cc26320919bb9747ddf7157af2323195e1fe3dba
yaoliusan <yaoliusan@gmail.com>
2020-10-16 06:48:53 +0000

    all implements

---
+++ src/com/nf163/todo/api/TaskArchiveApi.java
@@ -0,0 +1,24 @@
+package com.nf163.todo.api;
+
+import com.nf163.todo.dao.TaskDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/task/archive")
+public class TaskArchiveApi extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        try {
+            new TaskDAO().archiveTask(id);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("归档出错");
+        }
+    }
+}
---
+++ src/com/nf163/todo/api/TaskDelTodoApi.java
@@ -0,0 +1,25 @@
+package com.nf163.todo.api;
+
+
+import com.nf163.todo.dao.TaskDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/task/delTodo")
+public class TaskDelTodoApi extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        try {
+            new TaskDAO().deleteTodo(id);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("删除出错");
+        }
+    }
+}
